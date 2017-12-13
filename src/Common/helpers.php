e51deed9c918d92217d9ab2eb249912ba6b455a3
jacky-zeng <15900493964@163.com>
2017-12-13 16:04:30 +0800

    Update helpers.php

--- src/Common/helpers.php
+++ src/Common/helpers.php
@@ -9,30 +9,20 @@
  *
  * @return mixed
  */
-/*
 if (! function_exists('array_get')) {
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  \ArrayAccess|array  $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
     function array_get($array, $key, $default = null)
     {
-        if (is_null($key)) {
-            return $array;
-        }
-
-        if (isset($array[$key])) {
-            return $array[$key];
-        }
-
-        foreach (explode('.', $key) as $segment) {
-            if (! is_array($array) || ! array_key_exists($segment, $array)) {
-                return value($default);
-            }
-
-            $array = $array[$segment];
-        }
-
-        return $array;
+        return Arr::get($array, $key, $default);
     }
 }
-*/
 
 if (! function_exists('array_has')) {
     function array_has($array, $key)
