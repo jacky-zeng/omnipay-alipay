9e60cfbe7d26f88fb6de5ed4d7c46cc1b8821474
jacky <15900493964@163.com>
2017-12-14 09:22:01 +0800

    防止与laravel框架的array_get冲突

--- src/Common/helpers.php
+++ src/Common/helpers.php
@@ -12,23 +12,7 @@
 if (! function_exists('array_get')) {
     function array_get($array, $key, $default = null)
     {
-        if (is_null($key)) {
-            return $array;
-        }
-
-        if (isset($array[$key])) {
-            return $array[$key];
-        }
-
-        foreach (explode('.', $key) as $segment) {
-            if (! is_array($array) || ! array_key_exists($segment, $array)) {
-                return value($default);
-            }
-
-            $array = $array[$segment];
-        }
-
-        return $array;
+        return \Illuminate\Support\Arr::get($array, $key, $default);
     }
 }
 
